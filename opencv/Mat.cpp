3c22ff28a38e2e2c98393308c1bde4a7bdd0c4c8
WhiteCells <xuan2692659908@foxmail.com>
2024-11-12 19:14:03 +0800

    update

---
+++ opencv/Mat.cpp
@@ -0,0 +1,8 @@
+#include "opencv2/highgui.hpp"
+#include "opencv2/core/hal/interface.h"
+
+int main(int argc, char *argv[])
+{
+    cv::Mat mat(4, 4, cv::CV_8UC1, cv::Scalar(255));
+    return 0;
+}
